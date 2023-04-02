f0b3044a466feaa755ef378ab66ff6210da0402a
Deepa Elangovan <109533865+Deepa140826@users.noreply.github.com>
2023-04-02 21:08:22 +0530

    Update Best Time to Buy and Sell Stock II.cpp

--- Array/Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.cpp
+++ Array/Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& n) {
+        if(n.size()==0) return 0;
+        int ans=0;
+        int st=n[0],ed=n[0];
+        for(int i=0;i<n.size();i++){
+            if(n[i]>ed){
+                ed=n[i];
+            }
+            else{
+                ans+=(ed-st);
+                st=n[i];
+                ed=n[i];
+            }
+        }
+        if(ed>st)
+            ans+=(ed-st);
+        return ans;
+        
+    }
+};
