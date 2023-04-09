ae6b5132de7c9f94bfa9fa9f3249189727ed97e7
Deepa Elangovan <109533865+Deepa140826@users.noreply.github.com>
2023-04-09 20:36:05 +0530

    Update Candy.cpp

--- Array/Candy/Candy.cpp
+++ Array/Candy/Candy.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+public:
+    int candy(vector<int>& nums) {
+        int n=nums.size();
+        int candy=n;
+        int i=1;
+        while(i<n){
+            if(nums[i]==nums[i-1]){
+                i++;
+                continue;
+            }
+            int peak=0;
+            while(nums[i]>nums[i-1]){
+                peak++;
+                candy+=peak;
+                i++;
+                if(i==n) 
+                return candy;
+            }
+            int valley=0;
+            while(i<n&&nums[i]<nums[i-1]){
+                valley++;
+                candy+=valley;
+                i++;
+            }
+            candy-=min(peak,valley);
+        }
+        return candy;
+    }
+};
